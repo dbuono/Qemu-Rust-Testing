a3b161597938c9c0abbfc6fe7d4ec5b180979921
Daniele Buono <dbuono@us.ibm.com>
2020-11-18 18:38:17 +0000

    Initial version of the network setup and benchmarks
    Signed-off-by: Daniele Buono <dbuono@us.ibm.com>

---
+++ net/network-metrics-iperf3.sh
@@ -0,0 +1,330 @@
+#!/bin/bash
+#
+# Copyright (c) 2018 Intel Corporation
+# 
+# SPDX-License-Identifier: Apache-2.0
+#
+# Cut-down version of the kata metrics network test
+# Originally available at
+# https://github.com/kata-containers/tests/blob/master/metrics/network/network-metrics-iperf3.sh
+#
+# Changes by Daniele Buono
+# Copyright (c) 2020 IBM Corporation
+
+
+# Port number where the server will run
+server_pid=""
+server_log=""
+port="${port:-5201}"
+user="root"
+# Measurement time (seconds)
+transmit_timeout="${transmit_timeout:-30}"
+
+# Iperf server configuration
+# Set the TMPDIR to an existing tmpfs mount to avoid a 9p unlink error
+server_address=192.168.1.2
+
+function start_server() {
+	local ip=${1}
+	local command=${2}
+	local arguments=${3}
+	server_log=$(mktemp)
+	echo "Starting iperf server. Log saved in ${server_log}"
+	ssh ${user}@${ip} -t -t "${command} ${arguments}" >${server_log} 2>&1 & 
+	server_pid=$!
+}
+
+function server_running() {
+	if [[ ( -d /proc/$server_pid ) && ( -z `grep zombie /proc/$server_pid/status` ) ]]; then
+		echo "Server is running."
+		return 0
+	else
+		echo "Server is not running. Log of start_server follows:"
+		cat ${server_log}
+		rm ${server_log}
+		return 1
+	fi
+}
+
+function stop_server() {
+	kill $server_pid
+	rm ${server_log}
+}
+
+function die() {
+	echo ${1}
+	exit 1
+}
+
+# Test single direction TCP bandwith
+function iperf3_bandwidth() {
+	local TEST_NAME="$1"
+
+	# Verify server IP address
+	if [ -z "$server_address" ];then
+		die "server: ip address no found"
+	fi
+
+	# Start client
+	local client_command="iperf3 -J -c ${server_address} -t ${transmit_timeout}"
+
+	# Start server
+	result=$($client_command)
+
+	local bits_per_second=$(echo "$result" | jq '.end.sum_received.bits_per_second')
+	local total_bandwidth=$(echo "scale=2 ; $bits_per_second / 1000000" | bc)
+
+	echo "Test network bwd single direction TCP"	
+	echo "Bandwidth (Mbps): $total_bandwidth"
+}
+
+# Test jitter on single direction UDP
+function iperf3_jitter() {
+	local TEST_NAME="$1"
+
+	# Verify server IP address
+	if [ -z "$server_address" ];then
+		die "server: ip address no found"
+	fi
+
+	# Start server
+	local client_command="iperf3 -J -c ${server_address} -u -t ${transmit_timeout}"
+	result=$($client_command)
+
+	local total_jitter=$(echo "$result" | jq '.end.sum.jitter_ms')
+
+	echo "Test network jitter single direction UDP"	
+	echo "Jitter (ms): $total_jitter"
+}
+
+# This function parses the output of iperf3 execution
+function parse_iperf3_bwd() {
+	local TEST_NAME="$1"
+	local result="$2"
+
+	if [ -z "$result" ]; then
+		die "no result output"
+	fi
+
+ 	# Getting results
+	local rx_bwd=$(echo "$result" | jq '.end.sum_received.bits_per_second')
+	local rx_bwd_mbps=$(echo "scale=2 ; $rx_bwd / 1000000" | bc)
+	local tx_bwd=$(echo "$result" | jq '.end.sum_sent.bits_per_second')
+	local tx_bwd_mbps=$(echo "scale=2 ; $tx_bwd / 1000000" | bc)
+
+	echo "Test ${TEST_NAME}"	
+	echo "RX Bandwidth (Mbps): $rx_bwd_mbps"
+	echo "TX Bandwidth (Mbps): $tx_bwd_mbps"
+}
+
+# This function parses the output of iperf3 UDP execution, and
+# saves the receiver successful datagram value in the results.
+function parse_iperf3_pps() {
+	local TEST_NAME="$1"
+	local result="$2"
+
+	if [ -z "$result" ]; then
+		die "no result output"
+	fi
+
+	# Extract results
+	local lost=$(echo "$result" | jq '.end.sum.lost_packets')
+ 	local total=$(echo "$result" | jq '.end.sum.packets')
+	local notlost=$((total-lost))
+	local pps=$((notlost/transmit_timeout))
+
+	echo "Test ${TEST_NAME}"	
+	echo "Received rate (pps): $pps"
+	echo "Lost packets: $lost"
+	echo "Total packets: $total"
+	echo "Not lost packets: $notlost"
+}
+
+# This function launches a container that will take the role of
+# server, this is order to attend requests from a client.
+# In this case the client is an instance of iperf3 running in the host.
+function get_host_cnt_bwd() {
+	local cli_args="$1"
+
+	# Verify server IP address
+	if [ -z "$server_address" ];then
+		die "server: ip address no found"
+	fi
+
+	# client test executed in host
+	local output=$(iperf3 -J -c $server_address -t $transmit_timeout "$cli_args")
+
+	echo "$output"
+}
+
+# Run a UDP PPS test between two containers.
+# Use the smallest packets we can and run with unlimited bandwidth
+# mode to try and get as many packets through as possible.
+function get_cnt_cnt_pps() {
+	local cli_args="$1"
+
+	# Verify server IP address
+	if [ -z "$server_address" ];then
+		die "server: ip address no found"
+	fi
+
+	# and start the client container
+	local client_command="iperf3 -J -u -c ${server_address} -l 64 -b 0 ${cli_args} -t ${transmit_timeout}"
+	local output=$($client_command)
+
+	echo "$output"
+}
+
+# Run a UDP PPS test between the host and a container, with the client on the host.
+# Use the smallest packets we can and run with unlimited bandwidth
+# mode to try and get as many packets through as possible.
+function get_host_cnt_pps() {
+	local cli_args="$1"
+
+	# Verify server IP address
+	if [ -z "$server_address" ];then
+		die "server: ip address no found"
+	fi
+
+	# and start the client container
+	local output=$(iperf3 -J -u -c $server_address -l 64 -b 0 -t $transmit_timeout "$cli_args")
+
+	echo "$output"
+}
+
+# This test measures the bandwidth between a container and the host.
+# where the container take the server role and the iperf3 client lives
+# in the host.
+function iperf3_host_cnt_bwd() {
+	local TEST_NAME="network bwd host contr"
+	local result="$(get_host_cnt_bwd)"
+	parse_iperf3_bwd "$TEST_NAME" "$result"
+}
+
+# This test is similar to "iperf3_host_cnt_bwd", the difference is this
+# tests runs in reverse mode.
+function iperf3_host_cnt_bwd_rev() {
+	local TEST_NAME="network bwd host contr reverse"
+	local result="$(get_host_cnt_bwd "-R")"
+	parse_iperf3_bwd "$TEST_NAME" "$result"
+}
+
+# This tests measures the bandwidth using different number of parallel
+# client streams. (2, 4, 8)
+function iperf3_multiqueue() {
+	local TEST_NAME="network multiqueue"
+	local client_streams=("2" "4" "8")
+
+	for s in "${client_streams[@]}"; do
+		tn="$TEST_NAME $s"
+		result="$(get_host_cnt_bwd "-P $s")"
+		parse_iperf3_bwd "$tn" "$result"
+	done
+}
+
+# This test measures the packet-per-second (PPS) between the host and a container.
+# It uses the smallest (64byte) UDP packet streamed with unlimited bandwidth
+# to obtain the result.
+function iperf3_host_cnt_pps() {
+	local TEST_NAME="network pps host cnt"
+	local result="$(get_host_cnt_pps)"
+	parse_iperf3_pps "$TEST_NAME" "$result"
+}
+
+# This test measures the packet-per-second (PPS) between the host and a container.
+# It runs iperf3 in 'Reverse' mode.
+# It uses the smallest (64byte) UDP packet streamed with unlimited bandwidth
+# to obtain the result.
+function iperf3_host_cnt_pps_rev() {
+	local TEST_NAME="network pps host cnt rev"
+	local result="$(get_host_cnt_pps "-R")"
+	parse_iperf3_pps "$TEST_NAME" "$result"
+}
+
+function help {
+echo "$(cat << EOF
+Usage: $0 "[options]"
+	Description:
+		This script implements a number of network metrics
+		using iperf3.
+
+	Options:
+		-a      Run all tests
+		-b      Run all bandwidth tests
+		-h      Shows help
+		-j      Run jitter tests
+		-p      Run all PPS tests
+EOF
+)"
+}
+
+function main {
+	local OPTIND
+ 	while getopts ":abh:jpt:" opt
+	do
+		case "$opt" in
+		a)      # all tests
+			test_bandwidth="1"
+			test_jitter="1"
+			test_pps="1"
+			;;
+		b)      # bandwidth tests
+			test_bandwidth="1"
+			;;
+		h)
+			help
+			exit 0;
+			;;
+		j)      # Jitter tests
+			test_jitter="1"
+			;;
+		p)      # all PacketPerSecond tests
+			test_pps="1"
+			;;
+		\?)
+			echo "An invalid option has been entered: -$OPTARG";
+			help
+			exit 1;
+			;;
+		:)
+			echo "Missing argument for -$OPTARG";
+			help
+			exit 1;
+			;;
+		esac
+	done
+	shift $((OPTIND-1))
+
+	[[ -z "$test_bandwidth" ]] && \
+	[[ -z "$test_jitter" ]] && \
+	[[ -z "$test_pps" ]] && \
+		help && die "Must choose at least one test"
+
+	start_server ${server_address} "iperf3" "-s"
+	sleep 5
+	server_running
+	if [ "$?" != "0" ]; then
+		echo "There was an error starting the server in the VM"
+		die 1
+	fi
+
+	if [ "$test_bandwidth" == "1" ]; then
+ 		iperf3_bandwidth
+		iperf3_host_cnt_bwd
+ 		iperf3_host_cnt_bwd_rev
+		iperf3_multiqueue
+	fi
+
+	if [ "$test_jitter" == "1" ]; then
+		iperf3_jitter
+	fi
+
+	if [ "$test_pps" == "1" ]; then
+		iperf3_host_cnt_pps
+		iperf3_host_cnt_pps_rev
+	fi
+
+	stop_server
+}
+
+main "$@"
--- net/qemu-net.sh
+++ net/qemu-net.sh
@@ -1,13 +1,104 @@
-#usr: root, pwd: root
+#!/bin/sh
 
-/home/acarter/qemu-5.0.0/x86_64-softmmu/qemu-system-x86_64 \
-	-enable-kvm \
-	-drive file=/tmp/focal-server-cloudimg-amd64-disk-kvm.img,media=disk,if=virtio \
+# Default values
+qemu_bin=qemu-system-x86_64
+virtio_daemon_bin=
+virtio_daemon_type=QEMU
+cloudinit_img=
+boot_img=
+test_img=
+tapname=mytap2
+#tapname=tap2
+hostip=192.168.1.1
+netmask=255.255.255.0
+vhost_socket=""
+
+show_help () {
+        echo "Usage: ${0} [OPTION] ... [BOOT_IMAGE] [TEST_IMAGE]"
+	echo "Start qemu with a folder shared through virtiofsd"
+	echo "BOOT_IMAGE is mandatory and is the OS image to boot in the VM"
+	echo "Options:"
+	echo "\t-q\tPath to a qemu binary. Defaults to ${qemu_bin}"
+	echo "\t-d\tPath to a virtio-user daemon binary."
+	echo "\t-c\tPath to a cloud-init image. If not provided, no cloud-init image will be used"
+	echo "\t-v\tType of virtio backend to use. Can be either QEMU or CH (For Cloud-Hypervisor)"
+}
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+while getopts "h?q:d:v:c:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    q)  qemu_bin=$OPTARG
+	;;
+    d)  virtio_daemon_bin=$OPTARG
+	;;
+    c)  cloudinit_img=$OPTARG
+	;;
+    v)  virtio_daemon_type=$OPTARG
+	;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+if [ "$#" != "2" ]; then
+	show_help
+	exit 0
+fi
+
+boot_img=${1}
+test_img=${2}
+
+qemu_cmdline="-enable-kvm \
+	-drive file=${boot_img},media=disk,if=ide \
 	-nographic \
-	-drive file=/tmp/nocloud.iso,if=virtio -m 4095 \
-	-chardev socket,id=char0,path=/tmp/vhostqemu \
-	-m 1G \
-	-netdev vhost-user,chardev=chardev0,id=netdev0 \
-	-device virtio-net-pci,netdev=netdev0 
-	#-device vhost-user-fs-pci,queue-size=1024,chardev=char0,tag=myfs \
-	#-m 4G -object memory-backend-file,id=mem,size=4G,mem-path=/dev/shm,share=on -numa node,memdev=mem \
+	-nic user,model=virtio-net-pci,hostfwd=tcp::9022-:22 \
+	-m 4G -smp 4 \
+	-drive file=${test_img},if=ide"
+
+if [ "${cloudinit_img}" != "" ]; then
+	qemu_cmdline="${qemu_cmdline} -drive file=${cloudinit_img},if=ide"
+fi
+
+case "${virtio_daemon_type}" in
+	"QEMU")
+		echo "Will use internal QEMU virtio network device"
+		qemu_cmdline="${qemu_cmdline} -netdev tap,id=mynet0,ifname=${tapname},script=no,downscript=no -device virtio-net-pci,netdev=mynet0"
+		;;
+	"CH")
+		echo "Will use Cloud-Hypervisor vhost-user block device"
+		vhost_socket=$(mktemp)
+		${virtio_daemon_bin} --net-backend "socket=${vhost_socket},ip=${hostip},mask=${netmask},num_queues=2,queue_size=512,tap=${tapname}" &
+		qemu_cmdline="${qemu_cmdline} -chardev socket,id=char0,path=${vhost_socket} \
+			-netdev vhost-user,chardev=char0,id=vhusernet0,queues=2 \
+			-device virtio-net-pci,netdev=vhusernet0 \
+			-object memory-backend-file,id=mem,size=4G,mem-path=/dev/shm,share=on -numa node,memdev=mem"
+		;;
+	*)
+		echo "Unrecognized virtio-block daemon"
+		exit 1
+esac
+
+if [ "${virtio_daemon_type}" = "QEMU" ]; then
+	echo "Creating tap device ${tapname}"
+	#ip tuntap add name ${tapname} mode tap multi_queue
+	user=$(whoami)
+	sudo ip tuntap add dev ${tapname} mode tap user ${user}
+	sleep 5
+	sudo ifconfig ${tapname} ${hostip}
+	sleep 5
+fi
+
+${qemu_bin} ${qemu_cmdline}
+
+if [ "${virtio_daemon_type}" = "QEMU" ]; then
+	echo "Deleting tap device ${tapname}"
+	sudo ip tuntap del dev ${tapname} mode tap
+fi
